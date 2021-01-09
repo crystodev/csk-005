59fc32b8482a5fe0f1e6571c59bcf88323a0bda8
Crystodev <>
2021-01-09 15:21:29 +0100

    Modularize code

---
+++ tokutils.py
@@ -0,0 +1,75 @@
+"""
+Utility module for Cardano Token manipulation
+"""
+import os
+import subprocess
+from collections import defaultdict
+
+def calculate_tokens_balance(tokens):
+  """
+  compute total balance for each list of tokens (token,amount)
+  return a dict with total amount for each token
+  """
+  result = defaultdict(int)
+  for key, value in tokens:
+    result[key] += value
+  return result
+
+def create_policy(token_name, tokens_path):
+  """
+  create policy for token
+  """
+  policy_script=tokens_path+token_name+'/policy.script'
+  policy_vkey=tokens_path+token_name+'/policy.vkey'
+  policy_skey=tokens_path+token_name+'/policy.skey'
+  policy = {}
+  policy['policy_script'] = policy_script
+  policy['policy_vkey'] = policy_vkey
+  policy['policy_skey'] = policy_skey
+
+  # check if token exists
+  # if so, returns existing policy
+  if(os.path.exists(tokens_path+token_name)) :
+    print("Token exists : no policy created for token", token_name)
+    policy_id = subprocess.run(['cardano-cli', 'transaction', 'policyid', '--script-file', policy_script], capture_output=True)
+    policy['policy_id'] = policy_id.stdout.decode().replace('\n', '')
+    return policy
+
+  os.makedirs(tokens_path+token_name, mode=0o777, exist_ok=True)
+
+  rc = subprocess.run(['cardano-cli', 'address', 'key-gen', '--verification-key-file', policy_vkey, '--signing-key-file', policy_skey], capture_output=False)
+
+  # create policy script
+  keyhash = subprocess.run(['cardano-cli', 'address', 'key-hash', '--payment-verification-key-file', policy_vkey], capture_output=True, text=True)
+  data = {}
+  data['keyHash'] = keyhash.stdout.replace('\n', '')
+  data['type'] = 'sig'
+  with open(policy_script, 'w') as outfile:
+    json.dump(data, outfile)
+
+  # get policy id
+  policy_id = subprocess.run(['cardano-cli', 'transaction', 'policyid', '--script-file', policy_script], capture_output=True, text=True)
+
+  policy['policy_id'] = policy_id.stdout.replace('\n', '')
+
+  return policy
+
+def get_address(address_file):
+  """
+  get address from file
+  """
+  addr_file = open(address_file,'r')
+  address = addr_file.readlines()
+  return address[0]
+
+def get_protocol_parameters(network, protparams_file):
+  """
+  get protocol parameters
+  """
+  network_name = network['network']
+  network_magic = str(network['network_magic'])
+  network_era = network['network_era']
+  env_param = network['env']
+  subprocess.run(['cardano-cli', 'query', 'protocol-parameters', network_name, network_magic, network_era, '--out-file', protparams_file], \
+    capture_output=False, text=True, env=env_param)
+  return 
---
+++ transaction.py
@@ -0,0 +1,92 @@
+"""
+This modules provides some Cardano blockchain transaction tools
+"""
+import subprocess
+
+def split_list(tx):
+  """
+  split list in two with '+' as a separator
+  """
+  idx_list = [idx for idx, val in enumerate(tx) if val == '+']
+  idx_list1 = [idx+1 for idx, val in enumerate(tx) if val == '+']
+  size = len(tx)
+  tx = [tx[i: j] for i, j in
+      zip([0] + idx_list1, idx_list + 
+      ([size] if idx_list[-1] != size else []))]
+  return tx
+
+def build_mint_transaction(network, address, token, amount, policy_id, utxo, fee, out_file):
+  """
+  build mint transaction for token
+  """
+  network_era = network['network_era']
+  balances = utxo['balances']
+  balances['lovelace'] = balances['lovelace']-fee
+  tx_out=address
+  for key, value in balances.items():
+    tx_out=tx_out+'+'+str(value)+' '+key
+  tx_out = tx_out+' +'+str(amount)+' '+policy_id+'.'+token
+  mint = str(amount)+' '+policy_id+'.'+token
+  subprocess.run(['cardano-cli', 'transaction', 'build-raw', network_era, '--fee', str(fee), utxo['in_utxo'].split(' ')[0], \
+    utxo['in_utxo'].split(' ')[1], '--tx-out', tx_out, '--mint', mint, '--out-file', out_file], \
+    capture_output=False, text=True)
+  return
+
+def calculate_fees(network, address, token, amount, policy_id, utxo, protparams_file):
+  """
+  calculate fee for on chain transaction
+  """
+  draft_file = '/tmp/'+token+'.txbody-draft'
+  build_mint_transaction(network, address, token, amount, policy_id, utxo, 0, draft_file)
+
+  rc = subprocess.run(['cardano-cli', 'transaction', 'calculate-min-fee', '--tx-body-file', draft_file, '--tx-in-count', str(utxo['count_utxo']), \
+    '--tx-out-count', '1', '--witness-count', '1', '--byron-witness-count', '0', '--protocol-params-file', protparams_file], \
+    capture_output=True, text=True)
+  min_fee = int(rc.stdout.split(' ')[0])
+  return min_fee
+
+def get_utxo_from_wallet(network, address):
+  """
+  get utxo from wallet
+  """
+  network_name = network['network']
+  network_magic = str(network['network_magic'])
+  network_era = network['network_era']
+
+  env_param = network['env']
+  utxo = {}
+  tx_disp = subprocess.run(['cardano-cli', 'query', 'utxo', network_name, network_magic, network_era, '--address', address], \
+    capture_output=True, text=True, env=env_param)
+  tx_list = tx_disp.stdout.split('\n')
+  tx_list = [tx.split() for tx in tx_list[2:] if tx != ""]
+  utxo['in_utxo'] = " ".join(["--tx-in "+tx[0]+'#'+tx[1] for tx in tx_list])
+  utxo['count_utxo'] = len(tx_list)
+  tx_list = [tx[2:] for tx in tx_list]
+
+  tx_list = [split_list(tx) for tx in tx_list]
+  t_list = [y for x in tx_list for y in x]
+  utxo['tokens'] = [ (token[1],int(token[0])) for token in t_list]
+  return utxo
+
+def sign_transaction(network, skey, policy, ok_fee_file, sign_file):
+  """
+  sign transaction
+  """
+  network_name = network['network']
+  network_magic = str(network['network_magic'])
+  rc = subprocess.run(['cardano-cli', 'transaction', 'sign', network_name, network_magic, '--signing-key-file', skey, '--signing-key-file', policy['policy_skey'], \
+    '--script-file', policy['policy_script'], '--tx-body-file', ok_fee_file, '--out-file', sign_file], \
+    capture_output=False, text=True)
+  return
+
+def submit_transaction(network, sign_file):
+  """
+  submit signed transaction on the network
+  """
+  network_name = network['network']
+  network_magic = str(network['network_magic'])
+  env_param = network['env']
+  rc = subprocess.run(['cardano-cli', 'transaction', 'submit', network_name, network_magic, '--tx-file', sign_file], \
+    capture_output=False, text=True, env=env_param)
+  return
+
